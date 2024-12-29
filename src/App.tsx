701abcde224a369f9f56236260f3678dfa5b8172
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-12-29 16:58:44 +0900

    페이지 나누기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Layout from './components/layout';
+import Home from './routes/home';
+import Profile from './routes/profile';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+      {
+        
+      }
+    ]
+  }
+])
+
 function App() {
-  return <></>;
+  return (
+  <>
+    <RouterProvider router={router} />
+  </>
+  );
 }
 
 export default App;
